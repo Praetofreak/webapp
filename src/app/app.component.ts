7ae9c9ef58c7e3e507065a46b2d66bbb1147f6b5
Praetofreak <s.cherup@web.de>
2024-08-16 17:38:44 +0200

    Spendenformular 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/spendenformular/spendenformular.component.ts
+++ src/app/spendenformular/spendenformular.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-spendenformular',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './spendenformular.component.html',
   styleUrl: './spendenformular.component.scss'
 })
 export class SpendenformularComponent {
+  donationForm = this.fb.group( {
+    artDerKleidung: "",
+    krisenGebiet:"",
+  })
+  constructor(private fb: FormBuilder) {}
 
+  onSubmit(): void {
+    console.log("Registrierte Spende:", this.donationForm.value)
+  }
 }
