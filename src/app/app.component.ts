9452148592c0bfde27ddd7c2c7918591f8a5012e
Praetofreak <s.cherup@web.de>
2024-09-11 22:10:33 +0200

    Header Funktion, Adressfelder deaktiviert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Kleiderbrücke e.V.';
+  menuOpen=false;
+  toggleMenu(){
+    this.menuOpen=!this.menuOpen;
+  }
 }
--- src/app/spendenformular/spendenformular.component.ts
+++ src/app/spendenformular/spendenformular.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-spendenformular',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './spendenformular.component.html',
   styleUrl: './spendenformular.component.scss'
 })
@@ -19,12 +20,31 @@ export class SpendenformularComponent {
       nachName:[""],
       straße:[""],
       stadt:[""],
-      plz:[""],
+      plz:["", [this.zipCodeValidator(/0213/i)]],
     })
   })
-  constructor(private fb: FormBuilder) {}
+
+  // Deaktiviert Adressfeld bei Auswahl Übergabe an Geschäftsstelle
+  constructor(private fb: FormBuilder) {
+    this.spendenFormular.get('handover')?.valueChanges.subscribe((value) => {
+      if(value === 'geschäftsstelle') {
+        this.spendenFormular.get('adresse')?.disable();
+      } else {
+        this.spendenFormular.get('adresse')?.enable();
+      }
+    });
+  }
 
   onSubmit(): void {
     console.log("Registrierte Spende:", this.spendenFormular.value);
   }
+
+// Überprüfung Postleitzahl
+zipCodeValidator(zipCode: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = zipCode.test(control.value);
+    return forbidden ? {forbiddenZipCode: {value: control.value}} : null;
+  };
+}
+
 }
