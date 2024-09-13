4a0add93bf82766129d00d94e965478f48db03b8
Praetofreak <s.cherup@web.de>
2024-09-13 13:21:12 +0200

    Impressum erledigt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { HomeComponent } from './home/home.component';
 import { SpendenformularComponent } from './spendenformular/spendenformular.component';
 import { UnsereMissionComponent } from './unsere-mission/unsere-mission.component';
 import { ErfolgsseiteComponent } from './erfolgsseite/erfolgsseite.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
     { path: 'spendenformular', component: SpendenformularComponent },
     { path: 'unsere-mission', component: UnsereMissionComponent },
     { path: 'erfolgsseite', component: ErfolgsseiteComponent },
+    { path: 'impressum', component: ImpressumComponent },
     { path: '', component: HomeComponent },
 ];
